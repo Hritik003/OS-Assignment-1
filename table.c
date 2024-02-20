137b9c7525702204fed3182762331c5aa40ff5f0
LShivaRudra <shivarudralolla@gmail.com>
2024-02-20 10:59:32 +0530

    new branch

--- table.c
+++ table.c
@@ -10,51 +10,95 @@
 #define WRITE_END 1
 #define MAX_CUSTOMERS 5
 
+int pipe_handler[MAX_CUSTOMERS][2];
+
+void display_menu(){
+    printf("Menu: \n");
+    FILE *fptr;
+    fptr = fopen("menu.txt","r");
+    if (!fptr) {
+        perror("Error occured in opening the menu.txt file");
+        exit(1);
+    }
+    char cuisine[100];
+    while (fgets(cuisine, 100, fptr) != NULL) {
+        printf("%s", cuisine);
+    }
+    fclose(fptr);
+}
+
+void create_child_process(int i){
+        if(pipe(pipe_handler[i])==-1){
+            perror("Error in creating the pipe");
+            exit(1);
+        }
+
+        //creating a process
+        pid_t pid = fork();
+
+        if(pid == 0){
+            //child process
+            int order_num;
+            printf("Enter the serial number(s) of the item(s) to order from the menu. Enter -1 when done: \n");
+            while(scanf("%d",&order_num)&&order_num!=-1){
+                close(pipe_handler[i][READ_END]);
+                write(pipe_handler[i][WRITE_END],&order_num,sizeof(order_num));
+            }
+            close(pipe_handler[i][WRITE_END]);
+            exit(0);
+        }
+}
+
 int main(){
     int table_number;
     printf("Enter Table Number:");
     scanf("%d",&table_number);
 
-    int pipe_handler[MAX_CUSTOMERS][2];
-    
-
     //asking the number of customers
-    while(1){
-        int num_of_customers;
-        printf("Enter Number of Customers at Table (maximum no. of customers can be 5):");
-        scanf("%d",&num_of_customers);
+    
+    int num_of_customers;
+    printf("Enter Number of Customers at Table (maximum no. of customers can be 5):");
+    scanf("%d",&num_of_customers);
 
-        if(num_of_customers==-1)break;//exit
+    if(num_of_customers==-1)return 0;//exit
 
-        //creating customer processes and seperate pipe for customer and table process
+    //displaying the contents of the menu
+    display_menu();
 
-        for(int i=0;i<num_of_customers;i++){
-            if(pipe(pipe_handler[i]==-1)){
-                perror("Error in creating the pipe");
-                exit(1);
-            }
+    //creating customer processes and seperate pipe for customer and table process
 
+    // for(int i=0;i<num_of_customers;i++){
+    //     if(pipe(pipe_handler[i])==-1){
+    //         perror("Error in creating the pipe");
+    //         exit(1);
+    //     }
 
-            //creating a process
-            int pid = fork;
+    //     //creating a process
+    //     pid_t pid = fork();
 
-            if(pid == 0){
-                //child process
+    //     if(pid == 0){
+    //         //child process
+    //         int order_num;
+    //         printf("Enter the serial number(s) of the item(s) to order from the menu. Enter -1 when done: \n");
+    //         while(scanf("%d",&order_num)&&order_num!=-1){
+    //             close(pipe_handler[i][READ_END]);
+    //             write(pipe_handler[i][WRITE_END],&order_num,sizeof(order_num));
+    //         }
+    //         close(pipe_handler[i][WRITE_END]);
+    //         // exit(0);
+    //     }
+    //     else if(pid > 0){
+    //         //parent proces
 
-            }
-            else if(pid > 0){
-                //parent proces
+    //     }
+    //     else{
+    //         // perror("Error in forking a process for %dth customer on %dth table",i,table_number);
+    //         perror("Error in forking a process\n");
+    //     } 
+    // }
 
-            }
-            else{
-                perror("Error in forking a process for %dth customer on %dth table",i,table_number);
-            }
-
-        }
-
-
+    for(int i=0;i<num_of_customers;i++){
+        create_child_process(i);
+        wait(NULL);
     }
-
-
-
 }
