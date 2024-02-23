70acd245effc8b4ec5324f96fe58e2e0eef727ac
Hritik <hritikraj_03@outlook.com>
2024-02-23 14:38:10 +0530

    waiter fully fixed

--- waiter.c
+++ waiter.c
@@ -21,6 +21,70 @@ struct customer_data{
 
 struct customer_data orders[MAX_CUSTOMERS];
 
+void calculate_bill(int *shmptr,int *shmptr_hotel){
+        printf("Processing orders...\n");
+        int bill=0;
+        int i=1;
+        while (shmptr[i]!=-1) { 
+            switch (shmptr[i]) {
+                case 1:
+                    bill+=30;
+                    break;
+                case 2:
+                    bill+=40;
+                    break;
+                case 3:
+                    bill+=25;
+                    break;
+                case 4:
+                    bill+=30; 
+                    break;
+                default:
+                    printf("Invalid order number: %d\n", shmptr[i]);
+                    bill=-1; 
+                    break;
+            }
+            if (bill==-1) break; 
+            i++;
+        }
+        if (bill!=-1) {
+            printf("Total bill calculated: %d INR\n", bill);
+            shmptr[0]=bill;
+            shmptr_hotel[0]=bill;
+            shmptr_hotel[19]=9999;
+            
+            
+            
+        }
+         else {
+            printf("Invalid orders detected, bill not calculated.\n");
+            shmptr[0]=-404; 
+        }
+        shmptr[19]=8888;
+}
+void admin_Instructed_close(int *shmptr,int *shmptr_hotel){
+    printf("<-------------------MESSAGE FROM HOTEL MANAGER----------------------->\n");
+        printf("\n");
+        printf("Admin has instructed to close\n");
+        printf("\n");
+        memset(shmptr_hotel,0,SHM_SIZE);
+        shmptr[200]=911;//sending table to terminate
+        //end of order of waiter1
+        //telling table to terminate, since admin told to close
+
+        while(shmptr[0]!=911){
+            if(shmptr[19]==9999){
+                shmptr[19]=8888;
+                calculate_bill(shmptr,shmptr_hotel);
+            }
+            continue;
+        }
+        shmptr_hotel[200]=911;
+        printf("Terminating waiter, since Admin is closed");
+        
+}
+
+
 
 
 int main(){
@@ -28,13 +92,8 @@ int main(){
     printf("Enter Waiter ID:");
     scanf("%d",&waiter_id);
 
-    //printf("Check 0");
-    //shm segment
-
-    
     key_t key = ftok("table.c",waiter_id);//for table
     key_t key_hotel = ftok("hotelmanager.c",waiter_id);//for hotelmanager
-
 
     if(key==-1){
         printf("error in ftok");
@@ -66,7 +125,6 @@ int main(){
         exit(1);
     }
 
-
     shmptr = shmat(shmid, NULL, 0);
     if (shmptr== NULL) {
         perror("shmat");
@@ -78,74 +136,39 @@ int main(){
         perror("shmat");
         exit(1);
     }
-    //shmptr[100]=8888;
     memset(shmptr_hotel,0,SHM_SIZE);
 
-
 while (1) {
-    // printf("check1\n");
-    if(shmptr[19]==2222){
-        shmptr_hotel[19]=2222;
-        printf("Message from table: \nno customers in this table, passing the message to hotel manager\n");
-        exit(1);
-    }
-    if (shmptr[19]==9999) {
-        printf("Processing orders...\n");
-        int bill=0;
-        int i=1;
-        while (shmptr[i]!=-1) { 
-            switch (shmptr[i]) {
-                case 1:
-                    bill+=30;
-                    break;
-                case 2:
-                    bill+=40;
-                    break;
-                case 3:
-                    bill+=25;
-                    break;
-                case 4:
-                    bill+=30; 
-                    break;
-                default:
-                    printf("Invalid order number: %d\n", shmptr[i]);
-                    bill=-1; 
-                    break;
+    
+        if(shmptr[19]==2222){
+            shmptr_hotel[19]=2222;
+            printf("Message from table: \nno customers in this table, passing the message to hotel manager\n");
+            exit(1);
+        }
+        
+        if(shmptr_hotel[19]==1111){
+            printf("no existing customers, waiter terminating");
+            shmptr[19]=1111;
+            exit(1);
+        }
+        if (shmptr[19]==9999) {
+            calculate_bill(shmptr,shmptr_hotel);        
+        }
+        if(shmptr_hotel[0]==911){
+            admin_Instructed_close(shmptr,shmptr_hotel);
+            if (shmdt(shmptr)==-1) {
+                perror("error in detaching");
+                exit(1);
             }
-            if (bill==-1) break; 
-            i++;
         }
 
-        if (bill!=-1) {
-            printf("Total bill calculated: %d INR\n", bill);
-            shmptr[0]=bill;
-            shmptr_hotel[0]=bill;
-            shmptr_hotel[19]=9999;
-            
-        } else {
-            printf("Invalid orders detected, bill not calculated.\n");
-            shmptr[0]=-404; 
-        }
-        shmptr[19]=8888;
-        
-        
-
-        
-    }
-    else{
-        //nothing
-    }
-                
         sleep(1);
-        //  shmptr[0]=8888;
+
         if(shmptr[0]==500){
-            exit(1);
+            if (shmdt(shmptr)==-1) {
+                perror("error in detaching");
+                exit(1);
+            }
         }
     }
-
-    if (shmdt(shmptr)==-1) {
-            perror("error in detaching");
-            exit(1);
-        }
-
 }
